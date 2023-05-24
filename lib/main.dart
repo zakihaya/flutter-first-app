99a9cf22b376d0be907cd95f4e2937584971d94a
hayazaki <toru.hayazaki@gmail.com>
2023-05-24 13:57:12 +0900

    Like機能を追加する

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,17 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -39,6 +50,13 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
 
     return Scaffold(
       body: Center(
@@ -48,10 +66,22 @@ class MyHomePage extends StatelessWidget {
             Text('A random idea:'),
             BigCard(pair: pair),
             SizedBox(height: 10),
-            ElevatedButton(onPressed: () {
-              print('Pressed');
-              appState.getNext();
-            }, child: Text('Press me')),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: () {
+                  appState.getNext();
+                }, child: Text('Next')),
+              ],
+            ),
           ],
         ),
       ),
